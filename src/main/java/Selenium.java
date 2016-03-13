e1be6415cf9b249314b23ed309e282d0427c2c18
Olga M <lawyer.entuziasty@gmail.com>
2016-03-13 00:39:18 -0800

    Initial commit

---
+++ src/main/java/Selenium.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium {
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();    // Version 1.1 :: Firefox
+
+		String text_case_id = "TC-001.01";
+		String url = "http://www.learn2test.net";
+		String title_expected = "learn2test.net";
+		
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		String title_actual = driver.getTitle();
+
+		if (title_expected.equals(title_actual)) {
+			System.out.println("Test Case ID: \t\t" + text_case_id);
+			System.out.println("URL: \t\t\t" + url);
+			System.out.println("Title Expected: \t" + title_expected);
+			System.out.println("Title Actual: \t\t" + title_actual);
+			System.out.println("Test Case Result: \t" + "PASSED");
+		} else {
+			System.out.println("Test Case ID: \t\t" + text_case_id);
+			System.out.println("URL: \t\t\t" + url);
+			System.out.println("Title Expected: \t" + title_expected);
+			System.out.println("Title Actual: \t\t" + title_actual);
+			System.out.println("Test Case Result: \t" + "FAILED");
+		}
+		driver.quit();
+	}
+}
